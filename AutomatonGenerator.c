076025cc0f34c8960d463b0e495022a321dc799d
Ricardo Kim <hexadecimal.exe@gmail.com>
2016-09-22 01:19:19 -0300

    bugfix
    1.0 - file generation fix:
    
    list to improve:
    -name file choose.

--- AutomatonGenerator.c
+++ AutomatonGenerator.c
@@ -7,64 +7,59 @@
 #define cls system("cls")
 #define green system("color A")
 
-typedef struct{
+typedef struct {
 	int qtdAlfabeto;
 	int qtdEstados;
 	int e0;
 	int qtdEstadoFinal;
-	
-}AFD_M;
 
-int main( int argc, char const *argv[])
-{
+} AFD_M;
 
-	
-	setlocale(LC_ALL,"Portuguese");
-	
+int main(int argc, char const *argv[]) {
+
+	setlocale(LC_ALL, "Portuguese");
+
 	AFD_M x;
 	int bType;
 	char programName[100];
-	
-	int i,j; // variaveis de auxilio
-	
+
+	int i, j; // variaveis de auxilio
+
 	green;
 	cls;
-	
+
 	puts("São quantos simbolos?");
 	scanf("%d", &x.qtdAlfabeto);
 	jmp;
-	
+
 	char Alfabeto[x.qtdAlfabeto];
-	for(i = 0; i< x.qtdAlfabeto; i++)
-	{
-		printf("Simbolo %d: ",i);
-		scanf("%d",Alfabeto[i]);
+
+	for (i = 0; i < x.qtdAlfabeto; i++) {
+		printf("Simbolo %d: ", i);
+		scanf(" %c", &Alfabeto[i]);
 	}
 	jmp;
-		
+
 	puts("São quantos estados?");
-	scanf("%d",&x.qtdEstados);
+	scanf("%d", &x.qtdEstados);
 	jmp;
-	
+
 	puts("Qual estado inicial?");
-	scanf("%d",&x.e0);
+	scanf("%d", &x.e0);
 	jmp;
-	
+
 	puts("Quantos estados finais?");
-	scanf("%d",&x.qtdEstadoFinal);
+	scanf("%d", &x.qtdEstadoFinal);
 	jmp;
-	
+
 	int EstadoFinal[x.qtdEstadoFinal];
-	for(i = 0 ; i < x.qtdEstadoFinal ; i ++)
-	{
-		printf("Defina qual será o Estado Final %d: ", i );
-		scanf("%d",&EstadoFinal[i]);
+	for (i = 0; i < x.qtdEstadoFinal; i++) {
+		printf("Defina qual será o Estado Final: ");
+		scanf("%d", &EstadoFinal[i]);
 	}
-	
+
 	cls;
-	
-	
-	
+
 	puts("=========================================");
 	puts("||                                     ||");
 	puts("|| Guia de preenchimento               ||");
@@ -72,61 +67,73 @@ int main( int argc, char const *argv[])
 	puts("|| Inserir o número do próximo estado  ||");
 	puts("||                                     ||");
 	puts("========================================");
-	
+
 	int tbTransition[x.qtdEstados][x.qtdAlfabeto];
-	
-	for (i = 0 ; i < x.qtdEstados ; i++)
-	{
-		for(j = 0 ; j < x.qtdAlfabeto ; j++)
-		{
-			printf("Para o Estado e%d \nQuando receber simbolo \"%c\"\nDeve ir para qual estado?",i,Alfabeto[j]);
-			scanf("%d",&tbTransition[i][j]);
-			
+
+	for (i = 0; i < x.qtdEstados; i++) {
+		for (j = 0; j < x.qtdAlfabeto; j++) {
+			printf(
+					"Para o Estado e%d \nQuando receber simbolo \"%c\"\nDeve ir para qual estado?",
+					i, Alfabeto[j]);
+			scanf("%d", &tbTransition[i][j]);
+
 		}
 	}
-	
+
 	puts("Escolha o tipo de código:");
 	puts("1 - blocos de \"função\"");
 	puts("2 - saltos por \"goto\"");
-	scanf("%d",&bType);
+	scanf("%d", &bType);
 	jmp;
-	
+
 	puts("Qual será o nome do Programa?");
-	scanf("%s",programName);
-	
+	scanf("%s", programName);
+
 	char path[200];
-	strcat(path,"./");
-	strcat(path,programName);
-	
-	
+	path[0] = '.';
+	strcat(path, "/");
+	strcat(path, programName);
+
 	//===================================================
-	// pier aula
 	FILE *fp;
-	fopen(path,"wt");
-	fprintf(fp,"#include<stdio.h>");
-	fprintf(fp,"#include<stdlib.h>");
-	fprintf(fp,"#include<string.h>");
-	for(i = 0 ; i < x.qtdEstados ; i++)
-	{
-		fprintf(fp,"void e%d",i);
+//	fp = fopen(path,"wt");
+	fp = fopen("trab.c", "wt");
+
+	fprintf(fp, "#include<stdio.h>\n");
+	fprintf(fp, "#include<stdlib.h>\n");
+	fprintf(fp, "#include<string.h>\n");
+	for (i = 0; i < x.qtdEstados; i++) {
+		fprintf(fp, "void e%d();\n", i);
 	}
-	fprintf(fp,"int main( int argc, char const *argv[]){");
-	fprintf(fp,"}");
-	
-	
-	for(i = 0 ; i < x.qtdEstados ; i++)
-	{
-		for(j = 0 ; j < x.qtdAlfabeto ; j++)
-		{
-			if(tbTransition[i][j] == -1);
-			
-			if(tbTransition[i][j] == -1);
-			
-			if(tbTransition[i][j] == -1);
-			
-		}
+	fprintf(fp, "int main( int argc, char const *argv[]){\n");
+	fprintf(fp, "    char entrada[200];\n");
+	fprintf(fp, "    scanf(\"%s\",entrada);\n");
+	fprintf(fp, "    e%d();\n", x.e0);
+	fprintf(fp, "    return 0;\n");
+	fprintf(fp, "}\n");
+	for (i = 0; i < x.qtdEstados; i++) {
+		fprintf(fp, "e%d(){\n", i);
+		fprintf(fp, "if(entrada[i]=='\0')\n");
+
+		//inserir a lógica de construção dos blocos
+/*
+
+		 for(j = 0 ; j < x.qtdAlfabeto ; j++){
+			if(tbTransition[i][j] == -1){
+				fprintf(fp, "if(entrada[i]=='%c'){\n",tbTransition[i][j]);
+				fprintf(fp, "rejeita();\n");
+				fprintf(fp, "}\n");
+			}else {
+
+			}
+
+		 }
+*/
+
+		fprintf(fp, "}\n", i);
 	}
-	
-	
+
+	fclose(fp);
+
 	return 0;
 }
