0914841a213505c625546fa62f8173d9f6aaebd1
Ricardo Kim <hexadecimal.exe@gmail.com>
2016-09-25 19:43:42 -0300

    New Struture
    My main algorithm to deliver to the college

--- AutomatonGenerate/proj1.c
+++ AutomatonGenerate/proj1.c
@@ -6,9 +6,17 @@
 #define jmp puts("")
 #define cls system("cls")
 #define green system("color A")
+#define max 100
 
-#define input "../In/in.txt"
-#define output "../OutputGenerated/GeradoTeste.c"
+ #define input "../In/in.txt"
+ #define output "../OutputGenerated/GeradoTeste.c"
+
+/*
+#define input "in.txt"
+#define output "GeradoTeste.c"
+*/
+
+FILE *fp;
 
 typedef struct {
 	int qtdAlfabeto;
@@ -16,31 +24,36 @@ typedef struct {
 	int e0;
 	int qtdEstadoFinal;
 
+	char Alfabeto[max];
+	int EstadoFinal[max];
+
+	int bType;
+
+	int tbTransition[max][max];
+
+	char programName[max];
+
 } AFD_M;
 
-int main(int argc, char const *argv[]) {
+void cmGoto();
+void cmFunc();
 
+int main(int argc, char **argv) {
+
+	//freopen(input,"r",stdin);
 	setlocale(LC_ALL, "Portuguese");
-	freopen(input,"r",stdin);
 
 	AFD_M x;
-	int bType;
-	char programName[100];
+	int i, j;
 
-	int i, j; // variaveis de auxilio
-
-	//green;
-	//cls;
-
+	//inicio da interface de escolhas
 	printf("São quantos simbolos: ");
 	scanf("%d", &x.qtdAlfabeto);
 	jmp;
 
-	char Alfabeto[x.qtdAlfabeto];
-
 	for (i = 0; i < x.qtdAlfabeto; i++) {
 		printf("Simbolo %d: ", i);
-		scanf(" %c", &Alfabeto[i]);
+		scanf(" %c", &x.Alfabeto[i]);
 	}
 	jmp;
 
@@ -56,75 +69,78 @@ int main(int argc, char const *argv[]) {
 	scanf("%d", &x.qtdEstadoFinal);
 	jmp;
 
-	int EstadoFinal[x.qtdEstadoFinal];
 	for (i = 0; i < x.qtdEstadoFinal; i++) {
 		printf("Defina qual será o Estado Final: ");
-		scanf("%d", &EstadoFinal[i]);
+		scanf("%d", &x.EstadoFinal[i]);
 	}
 	jmp;
-//	cls;
 
-	puts("=========================================");
-	puts("||                                     ||");
-	puts("|| Guia de preenchimento               ||");
-	puts("||                                     ||");
-	puts("|| Inserir o número do próximo estado  ||");
-	puts("||                                     ||");
-	puts("||  -1 para não existente              ||");
-	puts("||                                     ||");
-	puts("========================================");
-
-	int tbTransition[x.qtdEstados][x.qtdAlfabeto];
-
-	for (i = 0; i < x.qtdEstados; i++,jmp) {
+	for (i = 0; i < x.qtdEstados; i++, jmp) {
 		for (j = 0; j < x.qtdAlfabeto; j++) {
 			printf("Estado \"e%d\"\nRecebe \"%c\"\nPróximo estado: ", i,
-					Alfabeto[j]);
-			scanf("%d", &tbTransition[i][j]);
+					x.Alfabeto[j]);
+			scanf("%d", &x.tbTransition[i][j]);
 
 		}
 	}
+	jmp;
 
 	puts("Escolha o tipo de código:");
 	puts("1 - blocos de \"função\"");
 	puts("2 - saltos por \"goto\"");
-	scanf("%d", &bType);
+	scanf("%d", &x.bType);
 	jmp;
 
 	puts("Qual será o nome do Programa?");
-	scanf("%s", programName);
-	int equal;
-	char path[200];
-	path[0] = '.';
-	strcat(path, "/");
-	strcat(path, programName);
+	scanf("%s", x.programName);
+	jmp;
 
-	//===================================================
-	FILE *fp;
-//	fp = fopen(path,"wt");
-	fp = fopen(output, "wt");
+	// Fim da interface de entrada.
 
+	//Gerar Arquivos
+
+	if (x.bType == 1) {
+		fp = fopen(output, "wt");
+		cmFunc(x);
+		fclose(fp);
+	}
+	if (x.bType == 2) {
+		fp = fopen(output, "wt");
+		cmGoto(x);
+		fclose(fp);
+	}
+
+	//Fim da geração
+
+	system("pause");
+	return 0;
+}
+
+void cmFunc(AFD_M bdFunc) {
+
+	int i, j, equal;
 	fprintf(fp, "#include<stdio.h>\n");
 	fprintf(fp, "#include<stdlib.h>\n");
 	fprintf(fp, "#include<string.h>\n");
-	for (i = 0; i < x.qtdEstados; i++) {
+	for (i = 0; i < bdFunc.qtdEstados; i++) {
 		fprintf(fp, "void e%d();\n", i);
 	}
 	fprintf(fp, "void aceita();\n");
 	fprintf(fp, "void rejeita();\n");
 	fprintf(fp, "char entrada[200];\n");
-	fprintf(fp, "int i = 0;\n\n\n\n");
+	fprintf(fp, "int i = 0;\n\n");
+
 	fprintf(fp, "int main( int argc, char const *argv[]){\n");
 	fprintf(fp, "\n");
 	fprintf(fp, "    scanf(\"%%s\",entrada);\n");
-	fprintf(fp, "    e%d();\n", x.e0);
+	fprintf(fp, "    e%d();\n", bdFunc.e0);
 	fprintf(fp, "    return 0;\n");
 	fprintf(fp, "}\n");
 
-	for (i = 0; i < x.qtdEstados; i++) {
+	for (i = 0; i < bdFunc.qtdEstados; i++) {
 
-		for (j = 0; j < x.qtdEstadoFinal; j++) {
-			if (i == EstadoFinal[j]) {
+		for (j = 0; j < bdFunc.qtdEstadoFinal; j++) {
+			if (i == bdFunc.EstadoFinal[j]) {
 				equal = j;
 				break;
 			} else {
@@ -132,27 +148,28 @@ int main(int argc, char const *argv[]) {
 			}
 		}
 
-
-		fprintf(fp, "e%d() {\n", i);
+		fprintf(fp, "void e%d() {\n", i);
 		fprintf(fp, "	if(entrada[i] == 'null') {\n");
-		if (i == EstadoFinal[equal]) {
-			fprintf(fp, "		aceita();\n");
-		} else {
-			fprintf(fp, "		rejeita();\n");
+		if (equal != -1) {
+			if (i == bdFunc.EstadoFinal[equal]) {
+				fprintf(fp, "		aceita();\n");
+			} else {
+				fprintf(fp, "		rejeita();\n");
+			}
 		}
 		fprintf(fp, "	}\n");
 
 		//inserir a lógica de construção dos blocos
 
-		for (j = 0; j < x.qtdAlfabeto; j++) {
-			if (tbTransition[i][j] == -1) {
-				fprintf(fp, "	if (entrada[i] == '%c') {\n", Alfabeto[j]);
+		for (j = 0; j < bdFunc.qtdAlfabeto; j++) {
+			if (bdFunc.tbTransition[i][j] == -1) {
+				fprintf(fp, "	if (entrada[i] == '%c') {\n", bdFunc.Alfabeto[j]);
 				fprintf(fp, "		rejeita();\n");
 				fprintf(fp, "	}\n");
 			} else {
-				fprintf(fp, "	if (entrada[i] == '%c') {\n", Alfabeto[j]);
+				fprintf(fp, "	if (entrada[i] == '%c') {\n", bdFunc.Alfabeto[j]);
 				fprintf(fp, "		i++;\n");
-				fprintf(fp, "		e%d();\n", tbTransition[i][j]);
+				fprintf(fp, "		e%d();\n", bdFunc.tbTransition[i][j]);
 				fprintf(fp, "	}\n");
 			}
 
@@ -160,17 +177,17 @@ int main(int argc, char const *argv[]) {
 
 		fprintf(fp, "}\n");
 	}
-	fprintf(fp, "aceita(){\n");
+	fprintf(fp, "void aceita(){\n");
 	fprintf(fp, "	puts(\"Aceita\");\n");
 	fprintf(fp, "	exit(0);\n");
 	fprintf(fp, "}\n\n");
 
-	fprintf(fp, "rejeita(){\n");
+	fprintf(fp, "void rejeita(){\n");
 	fprintf(fp, "	puts(\"Rejeita\");\n");
 	fprintf(fp, "	exit(0);\n");
 	fprintf(fp, "}\n");
+}
 
-	fclose(fp);
+void cmGoto(AFD_M x) {
 
-	return 0;
 }
--- OutputGenerated/GeradoTeste.c
+++ OutputGenerated/GeradoTeste.c
@@ -9,24 +9,20 @@ void rejeita();
 char entrada[200];
 int i = 0;
 
-
-
 int main( int argc, char const *argv[]){
 
     scanf("%s",entrada);
     e0();
     return 0;
 }
-e0() {
+void e0() {
 	if(entrada[i] == 'null') {
 		rejeita();
 	}
 	if (entrada[i] == 'a') {
-		i++;
 		rejeita();
 	}
 	if (entrada[i] == 'b') {
-		i++;
 		rejeita();
 	}
 	if (entrada[i] == 'c') {
@@ -34,12 +30,11 @@ e0() {
 		e1();
 	}
 }
-e1() {
+void e1() {
 	if(entrada[i] == 'null') {
 		rejeita();
 	}
 	if (entrada[i] == 'a') {
-		i++;
 		rejeita();
 	}
 	if (entrada[i] == 'b') {
@@ -51,7 +46,7 @@ e1() {
 		e2();
 	}
 }
-e2() {
+void e2() {
 	if(entrada[i] == 'null') {
 		aceita();
 	}
@@ -64,16 +59,15 @@ e2() {
 		e2();
 	}
 	if (entrada[i] == 'c') {
-		i++;
 		rejeita();
 	}
 }
-aceita(){
+void aceita(){
 	puts("Aceita");
 	exit(0);
 }
 
-rejeita(){
+void rejeita(){
 	puts("Rejeita");
 	exit(0);
 }
